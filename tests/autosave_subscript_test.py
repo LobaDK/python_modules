72587576ff2de2bc97fa8d3380268cb817cd0325
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-08-22 14:53:08 +0200

    Add main guard to autosave test script

--- tests/autosave_subscript_test.py
+++ tests/autosave_subscript_test.py
@@ -1,26 +1,29 @@
-from dataclasses import dataclass, field
-from sys import path as sys_path
+def main() -> None:
+    from dataclasses import dataclass, field
+    from sys import path as sys_path
 
-sys_path.append(".")
+    sys_path.append(".")
 
-from settings.settings_manager import SettingsManagerWithDataclass
+    from settings.settings_manager import SettingsManagerWithDataclass
+
+    @dataclass
+    class DataclassSection:
+        key: str = field(default="value")
+
+    @dataclass
+    class DefaultSettingsAsDataClass:
+        section: DataclassSection = field(default_factory=DataclassSection)
+
+    default_settings_as_Dataclass = DefaultSettingsAsDataClass()
+
+    settings_manager = SettingsManagerWithDataclass(
+        path="settings.json",
+        default_settings=default_settings_as_Dataclass,
+        autosave=True,
+    )
+
+    settings_manager.settings.section.key = "new value"
 
 
-@dataclass
-class DataclassSection:
-    key: str = field(default="value")
-
-
-@dataclass
-class DefaultSettingsAsDataClass:
-    section: DataclassSection = field(default_factory=DataclassSection)
-
-
-default_settings_as_Dataclass = DefaultSettingsAsDataClass()
-
-
-settings_manager = SettingsManagerWithDataclass(
-    path="settings.json", default_settings=default_settings_as_Dataclass, autosave=True
-)
-
-settings_manager.settings.section.key = "new value"
+if __name__ == "__main__":
+    main()
